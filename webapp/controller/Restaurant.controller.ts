a763a00748dd69f10f1871db36690f549057ee16
FIX-IGH <felix.wilke@infotec-ag.de>
2026-09-09 16:20:24 +0200

    Dishes issue

--- webapp/controller/Restaurant.controller.ts
+++ webapp/controller/Restaurant.controller.ts
@@ -2,6 +2,13 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import UIComponent from "sap/ui/core/UIComponent";
 import Route, { Route$PatternMatchedEvent } from "sap/ui/core/routing/Route";
+import History from "sap/ui/core/routing/History";
+import Model from "sap/ui/model/Model";
+import JSONModel from "sap/ui/model/json/JSONModel";
+import ObjectListItem from "sap/m/ObjectListItem";
+import { ListItemBase$PressEvent } from "sap/m/ListItemBase";
+import Context from "sap/ui/model/Context";
+import View from "sap/ui/core/mvc/View";
 
 /**
  * @namespace tp.example.controller
@@ -14,9 +21,41 @@ export default class Restaurant extends Controller {
 	}
 
 	onObjectMatched(event: Route$PatternMatchedEvent): void {
+		var order = this.getView()?.getModel("order") as JSONModel;
 		this.getView()?.bindElement({
-			path: "/" + window.decodeURIComponent( (event.getParameter("arguments") as any).restaurantPath),
+			path: "/" + window.decodeURIComponent((event.getParameter("arguments") as any).restaurantPath),
 			model: "test"
 		});
+		
+	}
+
+	onNavBack(): void {
+		const history = History.getInstance();
+		const previousHash = history.getPreviousHash();
+
+		if (previousHash !== undefined) {
+			window.history.go(-1);
+		} else {
+			const router = UIComponent.getRouterFor(this);
+			router.navTo("app", {}, true);
+		}
+	}
+	
+	onAddDish(oEvent : ListItemBase$PressEvent): void {
+		var order = this.getView()?.getModel("order") as JSONModel;
+		var oItem = oEvent.getSource();
+		var context = oItem.getBindingContext("test") as Context;
+		//order.setProperty("/Restaurant", context.getProperty("/Restaurant"));
+		var dishes = order.getProperty("/Dishes");
+		dishes.push(context.getProperty(""))
+		order.setProperty("/Dishes", dishes)
+		console.log(order);
+		this.getView().byId("DefaultIconButton").setText(order.getProperty("/Dishes").length)
+	}
+
+	getDishesLength(aDishes) {
+		console.log(aDishes)
+
+		return aDishes.length;
 	}
 };
---
+++ webapp/model/formatter.ts
@@ -0,0 +1,40 @@
+import ResourceBundle from "sap/base/i18n/ResourceBundle";
+import Controller from "sap/ui/core/mvc/Controller";
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
+
+class Rating {
+    Author : String
+    Message : String
+    numStars : number
+    Type : String
+    Date: Date
+}
+
+export default {
+    calcAverage(arr: Array<any>) {
+        if (arr != null) {
+            if (arr.length > 0) {
+                //console.log(arr)
+                //console.log(arr.reduce((accumulator, currentValue) => accumulator + parseInt(currentValue.Rating), 0)/(arr.filter(e => parseInt(e.Rating) > 0).length));
+
+                return arr.reduce((accumulator, currentValue) => accumulator + parseInt(currentValue.Rating), 0)/(arr.filter(e => parseInt(e.Rating) > 0).length);
+            }
+            else {
+                return 0
+            }
+        }
+    },
+
+    ratingDesc(arr: Array<any>) {
+        if (arr != null) {
+            if (arr.length > 0) {
+                //console.log(arr)
+                //console.log(arr.reduce((accumulator, currentValue) => accumulator + parseInt(currentValue.Rating), 0)/(arr.filter(e => parseInt(e.Rating) > 0).length));
+                return "Average rating of " + Math.round(arr.reduce((accumulator, currentValue) => accumulator + parseInt(currentValue.Rating), 0)/(arr.filter(e => parseInt(e.Rating) > 0).length) * 100) / 100  + "/5 based on " + (arr.filter(e => parseInt(e.Rating) > 0).length) + " reviews";
+            }
+        }
+        else {
+            return "No reviews yet"
+        }
+    }
+}
